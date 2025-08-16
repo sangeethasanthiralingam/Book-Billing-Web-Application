29a3b75a2ad14a6cc6a2a1c956649d656dfcc1bd
sangeethasanthiralingam <sangeethasanthiralingam@gmail.com>
2025-08-16 21:28:42 +0530

    AppContextListener

---
+++ BookShopBillingApp/src/util/AppContextListener.java
@@ -0,0 +1,34 @@
+package util;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // Deregister JDBC drivers
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            try {
+                DriverManager.deregisterDriver(driver);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        // Stop MySQL cleanup thread
+        try {
+            com.mysql.cj.jdbc.AbandonedConnectionCleanupThread.checkedShutdown();
+        } catch (Exception e) {
+            // Ignore if class not found
+        }
+    }
+}
